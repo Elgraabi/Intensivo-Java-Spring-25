813b5d6e23bc3c0d3722021a87e5be43b7b437bc
Gabriel Oliveira <silvatdb8@gmail.com>
2025-05-22 21:43:10 -0300

    feat: Move games on list

---
+++ aula/dslist/src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+	
+	// Atributos
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+	
+	// Metodos Especificos
+	
+	// Metodos Especiais
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	public Integer getDestinationIndex() {
+		return destinationIndex;
+	}
+	public void setDestinationIndex(Integer destinationIndex) {
+		this.destinationIndex = destinationIndex;
+	}
+	
+	
+}
